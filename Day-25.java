320a21d326a8bd9d803934b770a5033e43ccc485
Promodini L <118465893+Promodini15@users.noreply.github.com>
2023-01-31 23:35:40 +0530

    Create Day-25.java

---
+++ Day-25.java
@@ -0,0 +1,13 @@
+//Day-25
+//Program to find area of circle.
+import java.util.*;
+public class AreaOfCircle {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the radius");
+        float num=sc.nextFloat();
+        double area=3.14*num*num;
+        System.out.println("Area of circle is "+area);
+    }
+    
+}
