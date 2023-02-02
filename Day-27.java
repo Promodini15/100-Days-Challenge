d3d768de5527bc65be6935a14a4b3e2ad4069483
Promodini L <118465893+Promodini15@users.noreply.github.com>
2023-02-02 21:07:11 +0530

    Create Day-27.java

---
+++ Day-27.java
@@ -0,0 +1,11 @@
+//Day-27
+//Write a program to find the double of the given number without using arithmetic operator
+import java.util.*;
+public class DoubleNumber {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        int num=sc.nextInt();
+        System.out.println(num<<1);
+    }   
+}
