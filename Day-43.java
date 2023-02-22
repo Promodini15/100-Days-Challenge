ada3ac58846abae778f7359b9605b20257a0a62b
Promodini L <118465893+Promodini15@users.noreply.github.com>
2023-02-22 22:38:06 +0530

    Update Day-43.java

--- Day-43.java
+++ Day-43.java
@@ -7,7 +7,7 @@ public class ArrayType {
     System.out.println("enter size of  array");
         int n=sc.nextInt();
         int[] arr=new int[n];
-        System.out.println("enter array elemnts");
+        System.out.println("enter array elements");
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
