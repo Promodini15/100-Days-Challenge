4ce59edb2cb41f09089b7058db74b6159d7a100b
Promodini L <118465893+Promodini15@users.noreply.github.com>
2023-02-22 21:38:24 +0530

    Create Day-44.java

---
+++ Day-44.java
@@ -0,0 +1,26 @@
+//Day 44
+//Write Program to find number of even and odd elements in an array
+import java.util.*;
+public class EvenAndOdd {
+    public static void main(String srgs[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter size of  array");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.println("enter array elemnts");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int even=0,odd=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]%2==0){
+                even++;
+            }
+            else{
+                odd++;
+            }
+        }
+        System.out.println("Number of even elemnts: "+even);
+        System.out.println("Number of odd elements: "+odd);
+}
+}
