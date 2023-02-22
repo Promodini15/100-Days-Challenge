908dd229d2dea1c1c07c3c238ff25ebfd3e5ca32
Promodini L <118465893+Promodini15@users.noreply.github.com>
2023-02-22 22:19:15 +0530

    Create Day-46.java

---
+++ Day-46.java
@@ -0,0 +1,21 @@
+//Day 46 
+//Write Program to find sum of elements in an array
+import java.util.*;
+public class SumOfArray {
+    public static void main(String srgs[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter size of  array");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        int sum=0;
+        System.out.println("enter the elemnts");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+         for(int i=0;i<n;i++){
+            sum=sum+arr[i];
+        }
+         System.out.println(sum);
+      
+}
+}
