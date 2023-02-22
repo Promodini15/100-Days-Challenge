de2b8a1af97b018da6779e8fc93c349146a90c78
Promodini L <118465893+Promodini15@users.noreply.github.com>
2023-02-22 20:20:31 +0530

    Create Day-43.java

---
+++ Day-43.java
@@ -0,0 +1,31 @@
+//Day 43
+//Write Program to find the array type
+import java.util.*;
+public class ArrayType {
+    public static void main(String srgs[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter size of  array");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.println("enter array elemnts");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+    int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]%2==0){
+                count++;
+            }
+        }
+        if(count==0){
+            System.out.println("odd");
+        }
+        else if(count==n){
+        System.out.println("even");
+    }
+        else{
+            System.out.println("mixed");
+        }
+  
+}
+}
